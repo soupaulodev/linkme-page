cd637d030754eb93608b04df14ecda8701a3349f
paulomarquesdev <paulomarques.rs1@gmail.com>
2024-03-20 02:14:42 -0300

    Update ui

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { PiMagicWand } from "react-icons/pi";
 import "./App.css";
 import { MdContentCopy } from "react-icons/md";
 import { useState } from "react";
+import logo from "./assets/logo_linktree.svg";
 export default function App() {
   const [url, setUrl] = useState("");
   const [isShortened, setIsShortened] = useState(false);
@@ -12,18 +13,21 @@ export default function App() {
   };
   const handleShortenSubmit = (e: React.FormEvent) => {
     e.preventDefault();
+    setIsShortened(!isShortened);
     console.log(url);
   };
-  const copyToClipboard = () => {
-    navigator.clipboard.writeText("https://linktree.com/");
-    setIsShortened(true);
-  };
+  // const copyToClipboard = () => {
+  //   navigator.clipboard.writeText("https://linktree.com/");
+  //   setIsShortened(true);
+  // };
 
   return (
     <div className="flex w-screen h-screen items-center justify-center overflow-hidden bg-gradient-to-b from-white to-zinc-100 text-black antialiased">
-      <div className="w-full max-w-md flex flex-col items-center justify-center p-12 rounded-sm">
-        <h1 className="text-5xl font-bold mb-2">LinkTree</h1>
-        <h2 className="text-1xl font-normal mb-5">
+      <div className="w-full max-w-2xl flex flex-col items-center justify-center p-10 rounded-sm">
+        <div className="max-w-xl mb-2 ">
+          <img src={logo} alt="Logo LinkTree" className="w-full" />
+        </div>
+        <h2 className="text-xl mb-8">
           Nunca foi tão facil encurtar seus links
         </h2>
 
@@ -31,14 +35,14 @@ export default function App() {
           <div className="relative w-full">
             <form>
               <label htmlFor="url" className="sr-only">
-                link
+                Link
               </label>
               <input
                 type="url"
                 id="url"
                 name="url"
                 placeholder="Enter with your link"
-                className="w-full p-2 bg-zinc-100 border border-zinc-500 rounded-sm placeholder:text-zinc-500 placeholder:px-1 focus:text-zinc-950 focus:outline-none focus:border-zinc-800"
+                className="w-full p-3 bg-zinc-100 border border-zinc-500 rounded-sm placeholder:text-zinc-500 placeholder:px-1 focus:text-zinc-950 focus:outline-none focus:border-zinc-800"
                 value={url}
                 onChange={handleShorten}
               />
@@ -46,15 +50,18 @@ export default function App() {
 
             <PiMagicWand
               onClick={handleShortenSubmit}
-              className="absolute right-0 top-0 bottom-0 m-auto me-3 text-lg text-zinc-700 hover:text-zinc-950"
+              className="absolute right-0 top-0 bottom-0 m-auto me-4 text-2xl text-zinc-700 hover:text-zinc-950"
             />
           </div>
         ) : (
-          <div className="relative w-full" onClick={copyToClipboard}>
-            <span className="block w-full h-[2.625rem] p-2 bg-zinc-100 border border-zinc-500 rounded-sm placeholder:text-zinc-500 placeholder:px-1 focus:text-zinc-950">
-              {url}
+          <div className="relative w-full">
+            <span className=" w-full flex items-center px-4 py-6 h-[2.625rem] bg-zinc-100 border border-zinc-500 rounded-sm placeholder:text-zinc-500 placeholder:px-1 focus:text-zinc-950">
+              {url}aaaaaaa
             </span>
-            <MdContentCopy className="absolute right-0 top-0 bottom-0 m-auto me-4 text-lg text-zinc-700 hover:text-zinc-950" />
+            <MdContentCopy
+              className="absolute right-0 top-0 bottom-0 m-auto me-4 text-2xl text-zinc-700 hover:text-zinc-950"
+              onClick={handleShortenSubmit}
+            />
           </div>
         )}
       </div>
